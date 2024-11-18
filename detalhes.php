126547124c3b613bf0ebd803bdefafd2e92ed4dc
JhonattanVenturini <jhonattan.venturini@outlook.com>
2024-11-17 22:56:34 -0300

    alteração na detalhes

--- detalhes.php
+++ detalhes.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../CSS/detalhes.css">
-    <link rel="icon" type="image/x-icon" href="../ASSETS/LOGO.webp">
+    <link rel="stylesheet" href="./CSS/detalhes.css">
+    <link rel="icon" type="image/x-icon" href="./ASSETS/LOGO.webp">
     <title>Navio</title>
 </head>
 
@@ -21,7 +21,7 @@
                 <fieldset class="conteudo">
                     <h1 style="padding: 5px;"> Icon of the Seas</h1>
                     <h3 class="definicao">Ícone de férias em família</h3>
-                    <img src="../ASSETS/N01.webp" alt="" class="img" />
+                    <img src="./ASSETS/N01.webp" alt="" class="img" />
                     <p>
                         Um texto publicitário sobre o navio. Os navios são embarcações de grande porte projetadas para navegar em mares e oceanos, desempenhando papéis essenciais no transporte de pessoas, bens e recursos. Utilizados há milênios, eles evoluíram de simples barcos a vela para complexas estruturas movidas por motores modernos. Existem diferentes tipos de navios, como cargueiros, petroleiros, cruzeiros e navios militares, cada um adaptado a funções específicas. Além de serem pilares do comércio global, eles também desempenham um papel vital em atividades como exploração científica, turismo e operações de resgate. Suas dimensões impressionantes e tecnologias avançadas refletem a engenhosidade humana em domar as águas e conectar o mundo.
                     </p>
